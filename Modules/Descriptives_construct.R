4ba5e055acf7de6eaa0fadd2d4f8d57976b4ea26
JamesMacek <82337151+JamesMacek@users.noreply.github.com>
2021-06-12 10:09:56 -0400

    deletion_commit2

--- Modules/Descriptives_construct.R
+++ Modules/Descriptives_construct.R
@@ -25,14 +25,20 @@ na_home <- diag(as.matrix(tr_na_2002[, -1]))
 #Getting all the vars together 
 data2plot["Ag Home Share"] <- ag_home 
 data2plot["Non-Ag Home Share"] <- na_home
-data2plot["La2000"] <- emp_GDP_data["La2000"]
-data2plot["Ln2000"] <- emp_GDP_data["Ln2000"]
-data2plot["La2005"] <- emp_GDP_data["La2005"]
-data2plot["Ln2005"] <- emp_GDP_data["Ln2005"]
-data2plot["Landmass"] <- (1/1000)*as.matrix(emp_GDP_data["Landmass (sqm)"])
+data2plot["La2000"] <- emp_GDP_data$L_ag_2000
+data2plot["Ln2000"] <- emp_GDP_data$L_na_2000
+data2plot["La2005"] <- emp_GDP_data$L_ag_2005
+data2plot["Ln2005"] <- emp_GDP_data$L_na_2005
+data2plot["La2010"] <- emp_GDP_data$L_ag_2010
+data2plot["Ln2010"] <- emp_GDP_data$L_na_2010
+
+
+data2plot["Landmass"] <- (1/1000)*as.matrix(emp_GDP_data["landmass"])
 data2plot["Ag_Share_2000"] <- as.matrix(data2plot["La2000"])/(as.matrix(data2plot["La2000"]) + as.matrix(data2plot["Ln2000"]))
 data2plot["Ag_Na_Rel_spend"] <- as.matrix(data2plot["Ag Home Share"])/(as.matrix(data2plot["Non-Ag Home Share"]))
 data2plot["Emp_density_2000"] <- (as.matrix(data2plot["La2000"]) + as.matrix(data2plot["Ln2000"]))/(as.matrix(data2plot["Landmass"]))
+data2plot["Emp_density_2005"] <- (data2plot$La2005 + data2plot$Ln2005)/data2plot$Landmass
+data2plot["Emp_density_2010"] <- (data2plot$La2010 + data2plot$Ln2010)/data2plot$Landmass
 
 data2plot <- data.frame(data2plot[, -1], row.names=emp_GDP_data$province)
 
@@ -49,16 +55,17 @@ ggsave("fig1.png")
 #####################PART 2########################
 data2plot["Ag_share_2005"] <- as.matrix(data2plot["La2005"])/(as.matrix(data2plot["La2005"]) + as.matrix(data2plot["Ln2005"]))
 #Explore employment growth from 2000 - 2005 (and beyond) as fn of initial pop density
-data2plot["na_2005_growth"] <- emp_GDP_data["Ln2005"]/emp_GDP_data["Ln2000"] - 1*rep(1, nrow(data2plot))
-data2plot["ag_2005_growth"] <- emp_GDP_data["La2005"]/emp_GDP_data["La2000"] - 1*rep(1, nrow(data2plot))
-data2plot["na_2010_growth"] <- emp_GDP_data["Ln2010"]/emp_GDP_data["Ln2005"] - 1*rep(1, nrow(data2plot))
-data2plot["ag_2010_growth"] <- emp_GDP_data["La2010"]/emp_GDP_data["La2005"] - 1*rep(1, nrow(data2plot))
-data2plot["Emp_2005_growth"] <- (emp_GDP_data["La2005"] + emp_GDP_data["Ln2005"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"]) -  1*rep(1, nrow(data2plot))
-data2plot["Emp_2010_growth"] <- (emp_GDP_data["La2010"] + emp_GDP_data["Ln2010"])/(emp_GDP_data["La2005"] + emp_GDP_data["Ln2005"]) -  1*rep(1, nrow(data2plot))
-data2plot["Emp_tot_growth"] <- (emp_GDP_data["La2010"] + emp_GDP_data["Ln2010"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"]) -  1*rep(1, nrow(data2plot))
+data2plot["na_2005_growth"] <- data2plot$Ln2005/data2plot$Ln2000 - 1*rep(1, nrow(data2plot))
+data2plot["ag_2005_growth"] <- data2plot$La2005/data2plot$La2000 - 1*rep(1, nrow(data2plot))
+data2plot["na_2010_growth"] <- data2plot$Ln2010/data2plot$Ln2005 - 1*rep(1, nrow(data2plot))
+data2plot["ag_2010_growth"] <- data2plot$La2010/data2plot$La2005 - 1*rep(1, nrow(data2plot))
+data2plot["Emp_2005_growth"] <- (data2plot$La2005 + data2plot$Ln2005)/(data2plot$La2000 + data2plot$Ln2000) -  1*rep(1, nrow(data2plot))
+data2plot["Emp_2010_growth"] <- (data2plot$La2010 + data2plot$Ln2010)/(data2plot$La2005 + data2plot$Ln2005) -  1*rep(1, nrow(data2plot))
+data2plot["Emp_tot_growth"] <- (data2plot$La2010 + data2plot$Ln2010)/(data2plot$La2000 + data2plot$Ln2000) -  1*rep(1, nrow(data2plot))
+
 #Testing linear model
 emp_glm_2005 <- lm_robust(Emp_2005_growth ~ Emp_density_2000, data=data2plot)
-emp_glm_tot <- lm_robust(Emp_tot_growth ~ Emp_density_2000, data=data2plot)
+emp_glm_2010 <- lm_robust(Emp_2010_growth ~ Emp_density_2005, data=data2plot)
 
 ggplot(data2plot, aes(x=Emp_density_2000, y=Emp_2005_growth)) + geom_point() +
   geom_text(label=rownames(data2plot), nudge_x=0.1, nudge_y=0, check_overlap = F, alpha = 0.8, size = 3.5) +
@@ -68,16 +75,16 @@ ggsave("emp_growth_density.png")
 
 
 #Decompose growth rates into two components (Agriculture and Non-agriculture)
-data2plot["ag_emp_cont_2005"] <- (emp_GDP_data["La2005"] - emp_GDP_data["La2000"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"])
-data2plot["na_emp_cont_2005"] <- (emp_GDP_data["Ln2005"] - emp_GDP_data["Ln2000"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"])
-data2plot["ag_emp_cont_tot"] <- (emp_GDP_data["La2010"] - emp_GDP_data["La2000"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"])
-data2plot["na_emp_cont_tot"] <- (emp_GDP_data["Ln2010"] - emp_GDP_data["Ln2000"])/(emp_GDP_data["La2000"] + emp_GDP_data["Ln2000"])
+data2plot["ag_emp_cont_2005"] <- (data2plot$La2005 - data2plot$La2000)/(data2plot$La2000 + data2plot$Ln2000)
+data2plot["na_emp_cont_2005"] <- (data2plot$Ln2005 - data2plot$Ln2000)/(data2plot$La2000 + data2plot$Ln2000)
+data2plot["ag_emp_cont_2010"] <- (data2plot$La2010 - data2plot$La2005)/(data2plot$La2005 + data2plot$Ln2005)
+data2plot["na_emp_cont_2010"] <- (data2plot$Ln2010 - data2plot$Ln2005)/(data2plot$La2005 + data2plot$Ln2005)
 
 
 ag_glm_2005 <- lm_robust(ag_emp_cont_2005 ~ Emp_density_2000, data=data2plot)
 na_glm_2005 <-  lm_robust(na_emp_cont_2005 ~ Emp_density_2000, data=data2plot)
-ag_glm_tot <- lm_robust(ag_emp_cont_tot~Emp_density_2000, data=data2plot)
-na_glm_tot <-  lm_robust(na_emp_cont_tot ~ Emp_density_2000, data=data2plot)
+ag_glm_2010 <- lm_robust(ag_emp_cont_2010~Emp_density_2000, data=data2plot)
+na_glm_2010 <-  lm_robust(na_emp_cont_2010 ~ Emp_density_2000, data=data2plot)
 
 ggplot(data2plot, aes(x=Emp_density_2000, y=na_emp_cont_2005)) + geom_point() +
   geom_text(label=rownames(data2plot), nudge_x=0.1, nudge_y=0, check_overlap = F, alpha = 0.8, size = 3.5) +
@@ -90,24 +97,17 @@ ggplot(data2plot, aes(x=Emp_density_2000, y=ag_emp_cont_2005)) + geom_point() +
 ggsave("ag_cont_growth_density.png")
 
 
-##Outliers?
-data2plot_nob <- data2plot[!rownames(data2plot) %in% "Beijing", ]
 
 
 #####################################################################################
 
 ##Checking data implied structural change
 data2plot_noint <- data2plot[!row.names(data2plot) %in% "International",]
-Aggregate_Ag_Share_2000 <- colSums(as.matrix(data2plot_noint$
-                                               La2000))/(colSums(as.matrix(data2plot_noint$La2000)) + colSums(as.matrix(data2plot_noint$Ln2000)))
+Aggregate_Ag_Share_2000 <- colSums(as.matrix(data2plot_noint$La2000))/(colSums(as.matrix(data2plot_noint$La2000)) + colSums(as.matrix(data2plot_noint$Ln2000)))
 Aggregate_Ag_Share_2005 <- colSums(as.matrix(data2plot_noint$La2005))/(colSums(as.matrix(data2plot_noint$La2005)) + colSums(as.matrix(data2plot_noint$Ln2005))) #Fell 8%
 
 ##Checking Population Density Dispersion. Coefficient of variation of population density
-data2plot["Emp_density_2005"] <- (data2plot$La2005 + data2plot$Ln2005)/data2plot$Landmass
-data2plot["Emp_density_2010"] <- (emp_GDP_data$La2010 + emp_GDP_data$Ln2010)/data2plot$Landmass
 data2plot_noint <- data2plot[!row.names(data2plot) %in% "International",]
-
-
 #Should theoretically be population weighted but whatever (for now)
 empdens_disp_2000 <- sd(data2plot_noint$Emp_density_2000)/mean(data2plot_noint$Emp_density_2000)
 empdens_disp_2005 <- sd(data2plot_noint$Emp_density_2005)/mean(data2plot_noint$Emp_density_2005)
